32e804b2f1afebaad015ffd7ad7274d26858e930
Olivia Lee <mynameisolivialee@gmail.com>
2022-01-18 14:18:03 +1100

    testing

---
+++ FRC_Rapid_React_2022/src/main/include/subsystems/sub_drive_base.h
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+// hi
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+class sub_drive_base : public frc2::SubsystemBase {
+ public:
+  sub_drive_base();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+};
