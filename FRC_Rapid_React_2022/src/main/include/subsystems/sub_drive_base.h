ed11f1269250725b591a67bba5a08ca881d8f6cd
Olivia Lee <mynameisolivialee@gmail.com>
2022-01-18 14:20:28 +1100

    test

--- FRC_Rapid_React_2022/src/main/include/subsystems/sub_drive_base.h
+++ FRC_Rapid_React_2022/src/main/include/subsystems/sub_drive_base.h
@@ -1,11 +1,8 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-<<<<<<< HEAD
-
-=======
 // hi
->>>>>>> Starting_Test
+
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
