ef11441295bfc9400be605683da27ba389a97d0e
Olivia Lee <mynameisolivialee@gmail.com>
2022-01-22 10:52:12 +1100

    Testing added files
    Experimenting with C++

---
+++ FRC_Rapid_React_2022/src/main/cpp/commands/Test_command_drive.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Test_command_drive.h"
+
+Test_command_drive::Test_command_drive() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void Test_command_drive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void Test_command_drive::Execute() {}
+
+// Called once the command ends or is interrupted.
+void Test_command_drive::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool Test_command_drive::IsFinished() {
+  return false;
+}
